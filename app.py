6d5e266339f90d81572595e6d64ebcb69136e899
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-02-10 23:30:09 -0600

    [ADD]: Gráfica de la evolución del error

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from NeuronalNetwork import NeuronalNetwork
 
-neuronalNet = NeuronalNetwork('datasets/or.csv')
-neuronalNet.show_data_init()
+lambdas = [1, 1e-6, 10, 0.1, 0.01]
 
-print("\n--- Iniciando Entrenamiento ---")
+results = []
 
-# 2. Entrenar
-# Nota: Para la compuerta OR, a veces se requieren más épocas o un lambda ajustado
-results = neuronalNet.train(epochs=100, lambda_value=0.1)
+for lambda_value in lambdas:
+    neuronalNet = NeuronalNetwork('datasets/or.csv')
+    print(f"\n--- Iniciando Entrenamiento Lambda = {lambda_value} ---")
+    results.append(neuronalNet.train(epochs=500, lambda_value=lambda_value))
 
-# 3. Presentar Resultados con los nuevos métodos
-results.show_final_metrics()    # Texto en consola
-results.plot_learning_curve()   # Gráfica visual
+
+plt.figure(figsize=(12, 8))
+plt.plot(results[0].errors, label='Lambda = 1', color='blue')
+plt.plot(results[1].errors, label='Lambda = 1e-6', color='green')
+plt.plot(results[2].errors, label='Lambda = 10', color='red')
+plt.plot(results[3].errors, label='Lambda = 0.1', color='orange')
+plt.plot(results[4].errors, label='Lambda = 0.01', color='purple')
+plt.xlabel('Épocas')
+plt.ylabel('Error')
+plt.title('Evolución del Error por Lambda')
+plt.legend()
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.show() 
